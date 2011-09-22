43e76f094f6ab8316f01725511da4c1654e7a13d
Dmitry Puchkov <berowar@gmail.com>
2011-09-22 18:50:02 +0300

    model Comment

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,7 @@ class Article < ActiveRecord::Base
   belongs_to :author, :class_name => "User", :foreign_key => "author_id"
   belongs_to :group
 
+  has_many :comments
+
   validates :author_id, :group_id, :title, :body, :presence => true
 end
